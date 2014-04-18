e04123425be790532e9af3484dc20d83847771e5
Nat Webb <natrwebb@gmail.com>
2014-04-18 12:08:09 -0500

    year is working, working on tests

--- test/test_integration_cal.rb
+++ test/test_integration_cal.rb
@@ -145,4 +145,85 @@ Su Mo Tu We Th Fr Sa
 EOS
     assert_equal expected_output, actual_output
   end
+
+  def test_a_basic_year
+    actual_output = ''
+    IO.popen('./cal 2014', 'r+') do |pipe|
+      actual_output = pipe.read
+    end
+
+    expected_output = <<EOS
+                             2014
+
+      January               February               March
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+          1  2  3  4                     1                     1  
+ 5  6  7  8  9 10 11   2  3  4  5  6  7  8   2  3  4  5  6  7  8  
+12 13 14 15 16 17 18   9 10 11 12 13 14 15   9 10 11 12 13 14 15  
+19 20 21 22 23 24 25  16 17 18 19 20 21 22  16 17 18 19 20 21 22  
+26 27 28 29 30 31     23 24 25 26 27 28     23 24 25 26 27 28 29  
+                                            30 31                 
+       April                  May                   June
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+       1  2  3  4  5               1  2  3   1  2  3  4  5  6  7  
+ 6  7  8  9 10 11 12   4  5  6  7  8  9 10   8  9 10 11 12 13 14  
+13 14 15 16 17 18 19  11 12 13 14 15 16 17  15 16 17 18 19 20 21  
+20 21 22 23 24 25 26  18 19 20 21 22 23 24  22 23 24 25 26 27 28  
+27 28 29 30           25 26 27 28 29 30 31  29 30                 
+                                                                  
+        July                 August              September
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+       1  2  3  4  5                  1  2      1  2  3  4  5  6  
+ 6  7  8  9 10 11 12   3  4  5  6  7  8  9   7  8  9 10 11 12 13  
+13 14 15 16 17 18 19  10 11 12 13 14 15 16  14 15 16 17 18 19 20  
+20 21 22 23 24 25 26  17 18 19 20 21 22 23  21 22 23 24 25 26 27  
+27 28 29 30 31        24 25 26 27 28 29 30  28 29 30              
+                      31                                          
+      October               November              December
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+          1  2  3  4                     1      1  2  3  4  5  6  
+ 5  6  7  8  9 10 11   2  3  4  5  6  7  8   7  8  9 10 11 12 13  
+12 13 14 15 16 17 18   9 10 11 12 13 14 15  14 15 16 17 18 19 20  
+19 20 21 22 23 24 25  16 17 18 19 20 21 22  21 22 23 24 25 26 27  
+26 27 28 29 30 31     23 24 25 26 27 28 29  28 29 30 31           
+                      30                                          
+EOS
+    assert_equal expected_output, actual_output
+  end
+
+  def test_a_leap_year
+    skip
+    actual_output = ''
+    IO.popen('./cal 2000', 'r+') do |pipe|
+      actual_output = pipe.read
+    end
+
+    expected_output = <<EOS
+EOS
+    assert_equal expected_output, actual_output
+  end
+
+  def test_a_year_with_six_line_months
+    skip
+    actual_output = ''
+    IO.popen('./cal 2012', 'r+') do |pipe|
+      actual_output = pipe.read
+    end
+
+    expected_output = <<EOS
+EOS
+    assert_equal expected_output, actual_output
+  end
+
+  def test_a_year_with_four_line_months
+    skip
+    actual_output = ''
+    IO.popen('./cal 2015', 'r+') do |pipe|
+      actual_output = pipe.read
+    end
+
+    expected_output = <<EOS
+EOS
+    assert_equal expected_output, actual_output
+  end
 end
